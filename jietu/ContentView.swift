ac5d7b7bb316607f7bf3d24b55f2b456eec150a6
lemos <xfe1235@gmail.com>
2026-03-21 14:10:54 +0800

    Initial Commit

---
+++ jietu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jietu
+//
+//  Created by Alfred Jobs on 2026/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jietu/jietuApp.swift
@@ -0,0 +1,17 @@
+//
+//  jietuApp.swift
+//  jietu
+//
+//  Created by Alfred Jobs on 2026/3/21.
+//
+
+import SwiftUI
+
+@main
+struct jietuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
